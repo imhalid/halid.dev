b95620315c4b55e8faeaa23bb82e98464398703d
imhalid <imhalid@icloud.com>
2022-07-06 02:29:48 +0300

    recreate files

--- pages/blog/[...slug].jsx
+++ pages/blog/[...slug].jsx
@@ -1,46 +1,6 @@
-import { getMdxNode, getMdxPaths } from "next-mdx/server";
-import { useHydrate } from "next-mdx/client";
-import { mdxComponents } from "../../components/mdxComponents";
-
 import Layouts from "../../components/Layouts";
-const PostPage = ({ post }) => {
-  console.log(post);
-  const content = useHydrate(post, {
-    components: mdxComponents,
-  });
-  return (
-    <Layouts>
-      <article>
-        <h1>{post.frontMatter.title}</h1>
-        <p>{post.frontMatter.except}</p>
-        <hr />
-      </article>
-      <div className="prose">{content}</div>
-    </Layouts>
-  );
+const PostPage = () => {
+  return <Layouts>dg∂</Layouts>;
 };
 
 export default PostPage;
-
-export async function getStaticPaths() {
-  return {
-    paths: await getMdxPaths("post"),
-    fallback: false,
-  };
-}
-
-export async function getStaticProps(context) {
-  const post = await getMdxNode("post", context);
-
-  if (!post) {
-    return {
-      notFound: true,
-    };
-  }
-
-  return {
-    props: {
-      post,
-    },
-  };
-}
--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,40 +1,17 @@
 import Layouts from "../../components/Layouts";
-import { getAllNodes } from "next-mdx/server";
+
 import Head from "next/head";
 import Link from "next/link";
 //https://github.com/shadcn/next-mdx
-const Blog = ({ posts }) => {
-  console.log(posts);
+const Blog = () => {
   return (
     <Layouts>
       <Head>
         <title>Blog</title>
       </Head>
-      <div className="space-y-6">
-        {posts.map((post) => {
-          return (
-            <article key={post.url}>
-              <h1>
-                <Link href={post.url}>
-                  <a className="text-xl font-bold">{post.frontMatter.title}</a>
-                </Link>
-              </h1>
-              <p>{post.frontMatter.excerpt}</p>
-              <p className="text-gray-400">{post.frontMatter.date}</p>
-            </article>
-          );
-        })}
-      </div>
+      <div className="space-y-6"></div>
     </Layouts>
   );
 };
 
 export default Blog;
-
-export async function getStaticProps() {
-  return {
-    props: {
-      posts: await getAllNodes("post"),
-    },
-  };
-}
