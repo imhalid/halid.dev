9fb8b9d459506ef69ff2a58da5b5fccaf93d782b
imhalid <imhalid@icloud.com>
2022-07-17 18:53:58 +0300

    og image test

--- util/createOgImage.jsx
+++ util/createOgImage.jsx
@@ -10,13 +10,13 @@ export const createOgImage = ({ title, meta }) =>
 
     // TITLE
     // Karla google font in light rose
-    `l_text:Karla_72_bold:${e(title)},co_rgb:ffe4e6,c_fit,w_1400,h_240`,
+    `l_text:Karla_72_bold:${e(title)},co_rgb:ffffff,c_fit,w_1400,h_240`,
     // Positioning
     `fl_layer_apply,g_south_west,x_100,y_180`,
 
     // META
     // Karla, but smaller
-    `l_text:Karla_48:${e(meta)},co_rgb:ffe4e680,c_fit,w_1400`,
+    `l_text:Karla_48:${e(meta)},co_rgb:ffffff80,c_fit,w_1400`,
     // Positioning
     `fl_layer_apply,g_south_west,x_100,y_100`,
 
@@ -29,5 +29,5 @@ export const createOgImage = ({ title, meta }) =>
     `fl_layer_apply,w_140,g_north_west,x_100,y_100`,
 
     // BG
-    `ogImageBG.webp`,
+    `ogBgImage.jpg`,
   ].join("/");
