7d62844948e177b8ff553305941ca2aafb8880bf
imhalid <imhalid@icloud.com>
2022-07-17 04:53:07 +0300

    og image test

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
 import { AnimatePresence } from "framer-motion";
 import { ThemeProvider } from "next-themes";
+import { DefaultSeo } from "next-seo";
+import { seo } from "../util/seo";
 
 function MyApp({ Component, pageProps, router }) {
   return (
     <>
       <ThemeProvider attribute="class">
         <AnimatePresence exitBeforeEnter initial={false}>
+          <DefaultSeo {...seo} />
           <Component {...pageProps} key={router.route} />
         </AnimatePresence>
       </ThemeProvider>
