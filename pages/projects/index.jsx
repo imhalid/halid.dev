c4b441cfd5296e2bc04ef5b199ca210c3dacb8f1
imhalid <imhalid@icloud.com>
2022-07-19 01:10:32 +0300

    create projects page

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -4,28 +4,6 @@ import classNames from "../../util/classNames";
 // import { useState, useEffect } from "react";
 import Head from "next/head";
 
-/*
-
-export const getStaticProps = async () => {
-  const { data: navbarData } = await client.query({
-    query: navbar,
-  });
-
-  const { data: footerData } = await client.query({
-      query: footer,
-  });
-
-  return {
-    props: {
-      headermenu: navbarData.menu.edges,
-      footer: navBarFooter.foo.bar
-    },
-    revalidate: 1,
-  };
-};`
-
-*/
-
 export const getStaticProps = async () => {
   const [userRes, repoRes] = await Promise.all([
     fetch("https://api.github.com/users/imhalid"),
@@ -38,59 +16,41 @@ export const getStaticProps = async () => {
   return { props: { user, repos } };
 };
 
-// export const getStaticProps = async () => {
-//   const res = await fetch("https://api.github.com/users/imhalid");
-//   const user = await res.json();
-//   if (res.status !== 200) {
-//     console.error(user);
-//     throw new Error("Failed to fetch API");
-//   }
-//   return {
-//     props: {
-//       user,
-//     },
-//     // Next.js will attempt to re-generate the page:
-//     // - When a request comes in
-//     // - At most once every 10 seconds
-//     revalidate: 10, // In seconds
-//   };
-// };
-
 const Projects = ({ user, repos }) => {
   console.log(user);
   console.log(repos);
-
-  // const [repos, setRepos] = useState("");
-  // const [user, setUser] = useState("");
-
-  // const getUserRepos = async (username) => {
-  //   const response = await fetch(
-  //     `https://api.github.com/users/${username}/repos`
-  //   );
-  //   const data = await response.json();
-  //   console.log(data);
-  //   setRepos(data);
-  // };
-
-  // const getUser = async (username) => {
-  //   const response = await fetch(`https://api.github.com/users/${username}`);
-  //   const data = await response.json();
-  //   setUser(data);
-  //   console.log(data);
-  // };
-
-  // console.log(user);
-
-  // useEffect(() => {
-  //   getUserRepos("imhalid");
-  //   getUser("imhalid");
-  // }, []);
 
   return (
     <div>
       <Head>
         <title>Projects</title>
       </Head>
+      <Layouts>
+        <img src={user.avatar_url} alt="" />
+        <h1>{user.name}</h1>
+        <p>{user.bio}</p>
+        <p>{user.hireable ? "hireable" : "injob"}</p>
+        <p>{user.created_at} created date</p>
+        <p>{user.followers} followers</p>
+        <p>{user.following} following</p>
+        <p>{user.public_repos} public repos</p>
+        <a href={user.html_url}>link</a>
+        <p>{user.updated_at} updated at</p>
+
+        <hr />
+        {repos.map((repo) => (
+          <div key={repo.id}>
+            <hr />
+            <h2>{repo.name} repos name</h2>
+            <p>{repo.description} description</p>
+            <p>{repo.language} language</p>
+            <p>{repo.created_at} created_at</p>
+            <p>{repo.size} size</p>
+            <a href={repo.html_url}>link</a>
+            <p>{repo.stargazers_count}</p>
+          </div>
+        ))}
+      </Layouts>
     </div>
   );
 };
