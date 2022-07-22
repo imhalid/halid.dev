5e39cbc260b79cf7d71282f28ab6aed464809268
imhalid <imhalid@icloud.com>
2022-07-22 19:31:08 +0300

    projects page edit

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -1,57 +1,74 @@
 import Link from "next/link";
+import Head from "next/head";
+import Layouts from "../../components/Layouts";
 import { TbGitFork } from "react-icons/tb";
 import { FaSort } from "react-icons/fa";
+import { AiFillStar } from "react-icons/ai";
 import cx from "clsx";
 import classNames from "../../util/classNames";
-import { AiFillStar } from "react-icons/ai";
-import Layouts from "../../components/Layouts";
+
 import GithubProfileCard from "../../components/githubProfileCard";
-import { useState } from "react";
-import Head from "next/head";
+import { useState, useEffect } from "react";
 
-export const getStaticProps = async () => {
-  const url = "https://api.github.com/users/";
-  const [userRes, repoRes] = await Promise.all([
-    fetch(url + "imhalid"),
-    fetch(url + "imhalid" + "/repos"),
-  ]);
-  const [user, repos] = await Promise.all([userRes.json(), repoRes.json()]);
-  if (userRes.status !== 200 || repoRes.status !== 200) {
-    console.log("error");
-  }
-  return { props: { user, repos } };
-};
-
-const Projects = ({ user, repos }) => {
+const Projects = () => {
   // console.log(user);
   // console.log(repos);
 
   // sorted by updated date
-  const [sort, setSort] = useState(true);
-
-  repos.sort((a, b) => {
-    if (sort) {
-      return (
-        parseInt(b.updated_at.split("-")[1]) -
-        parseInt(a.updated_at.split("-")[1])
-      );
-    } else {
-      return b.stargazers_count - a.stargazers_count;
-    }
-  });
 
   // const myTime = "16:59:17";
   // const d = new Date();
   // let hour = d.toLocaleTimeString();
   // console.log(parseInt(hour.slice(0, 5)) - parseInt(myTime.slice(0, 5)));
 
-  const timeSplit = (x) => {
-    //delete T and Z and replace with " "
-    const time = x.replace("T", " ").replace("Z", " ");
-    const splitTime = time.split(" ");
-    // console.log(sliceTime);
-    return splitTime[0];
+  // const timeSplit = (x) => {
+  //   //delete T and Z and replace with " "
+  //   const time = x.replace("T", " ").replace("Z", " ");
+  //   const splitTime = time.split(" ");
+  //   // console.log(sliceTime);
+  //   return splitTime[0];
+  // };
+
+  const [repos, setRepos] = useState("");
+  const [user, setUser] = useState("");
+
+  const getUserRepos = async (username) => {
+    const response = await fetch(
+      `https://api.github.com/users/${username}/repos`
+    );
+    const data = await response.json();
+
+    setRepos(data);
   };
+
+  const getUser = async (username) => {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const data = await response.json();
+    setUser(data);
+  };
+
+  useEffect(() => {
+    getUserRepos("imhalid");
+    getUser("imhalid");
+  }, []);
+
+  const sortRepos = () => {
+    console.log("sort");
+    const sortedRepos = [...repos].sort((a, b) => {
+      return (
+        parseInt(b.updated_at.split("-")[1]) -
+        parseInt(a.updated_at.split("-")[1])
+      );
+      setRepos(sortedRepos);
+    });
+  };
+
+  if (!user || !repos) {
+    return;
+    <Layouts>
+      <div>Loading...</div>
+    </Layouts>;
+  }
 
   return (
     <div>
@@ -70,12 +87,9 @@ const Projects = ({ user, repos }) => {
             url={user.html_url}
             repos={user.public_repos}
           />
-          <div
-            className="text-center cursor-pointer"
-            onClick={() => setSort(!sort)}
-          >
+          <div className="text-center cursor-pointer" onClick={sortRepos()}>
             <p className=" bg-blue-100 dark:bg-zinc-900/70 border  dark:border-neutral-700/50 border-neutral-400/20 py-2 rounded-md">
-              {sort ? "Sort by star" : "Sort by date"}{" "}
+              Sort by star
               <FaSort className="inline" />
             </p>
           </div>
@@ -118,7 +132,7 @@ const Projects = ({ user, repos }) => {
                   </span>
                 </div>
                 <p className="dark:text-gray-400 text-gray-600">
-                  {timeSplit(repo.created_at)}
+                  {/* {timeSplit(repo.created_at)} */}
                 </p>
               </div>
             </div>
